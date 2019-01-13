8de8ea63ae75d7cbc7c396311a4c2f73367e5e7b
Joel <jvgille@users.noreply.github.com>
2019-01-13 11:13:11 +0100

    replace png crate with image

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-extern crate png;
 extern crate termion;
+extern crate image;
 
+use image::*;
 use termion::color;
-use std::fs::File;
 
 fn set_bg(r : u8, g : u8, b : u8) {
     print!("{}", color::Bg(color::Rgb(r, g, b)));
@@ -12,11 +12,10 @@ fn set_fg(r : u8, g : u8, b : u8) {
     print!("{}", color::Fg(color::Rgb(r, g, b)));
 }
 
-fn calc_downscale(info: &png::OutputInfo) -> u32 {
+fn calc_downscale(image: &DynamicImage) -> u32 {
     let (t_width, t_height) = termion::terminal_size().unwrap();
-    let (t_width, t_height) = (t_width as f64, (t_height*2) as f64);
-    let h_scale = (info.width as f64/t_width).ceil();
-    let v_scale = (info.height as f64/t_height).ceil();
+    let h_scale = (image.width() as f64/t_width as f64).ceil();
+    let v_scale = (image.height() as f64/(t_height*2) as f64).ceil();
     if h_scale > v_scale {
         h_scale as u32
     } else {
@@ -24,25 +23,27 @@ fn calc_downscale(info: &png::OutputInfo) -> u32 {
     }
 }
 
-fn get_pixel(buf: &Vec<u8>, info: &png::OutputInfo, x: u32, y: u32) -> (u8, u8, u8, u8) {
-    let base = ((y*info.width + x) * (info.color_type.samples() as u32)) as usize;
+// gets pixel by x, y coordinate from rgba image buffer
+fn get_pixel(buf: &Vec<u8>, image: &DynamicImage, x: u32, y: u32) -> (u8, u8, u8, u8) {
+    let base = ((y*image.width() + x) * 4) as usize;
     (buf[base], buf[base+1], buf[base+2], buf[base+3])
 }
 
-fn sample(buf: &Vec<u8>, info: &png::OutputInfo, x: u32, y: u32, size: u32) -> (u8, u8, u8) {
+fn sample(buf: &Vec<u8>, image: &DynamicImage, x: u32, y: u32, size: u32) -> (u8, u8, u8) {
     let (mut r, mut g, mut b) = (0., 0., 0.);
     for i in x..x+size {
         for j in y..y+size {
-            let temp = get_pixel(&buf, &info, i, j);
-            let alpha = temp.3 as f64 / 255 as f64;
+            let temp = get_pixel(&buf, &image, i, j);
+            let alpha = temp.3 as f64 / 255.;
             r += alpha * (temp.0 as f64);
             g += alpha * (temp.1 as f64);
             b += alpha * (temp.2 as f64);
         }
     }
-    r /= (size*size) as f64;
-    g /= (size*size) as f64;
-    b /= (size*size) as f64;
+    let size_2 = (size*size) as f64;
+    r /= size_2;
+    g /= size_2;
+    b /= size_2;
     (r as u8, g as u8, b as u8)
 }
 
@@ -56,21 +57,19 @@ fn main() {
         std::process::exit(1);
     }
 
-    let decoder = png::Decoder::new(File::open(&args[1]).unwrap());
-    let (info, mut reader) = decoder.read_info().unwrap();
-    let mut buf = vec![0; info.buffer_size()];
-    reader.next_frame(&mut buf).unwrap();
+    // todo need to convert, image.to_rgba()
+    let image = image::open(&args[1]).unwrap();
+    let buf = image.raw_pixels();
+    let scale = calc_downscale(&image);
 
-    let scale = calc_downscale(&info);
-
-    println!("Dimensions: {}x{}", info.width, info.height);
+    println!("Dimensions: {}x{}", image.width(), image.height());
 
     let mut last_y = 0;
-    for y in (0..info.height - 2 * scale + 1).step_by(scale as usize * 2) {
-        for x in (0..info.width - scale).step_by(scale as usize) {
-            let (r, g, b) = sample(&buf, &info, x, y, scale);
+    for y in (0..image.height() - 2 * scale + 1).step_by(scale as usize * 2) {
+        for x in (0..image.width() - scale).step_by(scale as usize) {
+            let (r, g, b) = sample(&buf, &image, x, y, scale);
             set_bg(r, g, b);
-            let (r, g, b) = sample(&buf, &info, x, y + scale, scale);
+            let (r, g, b) = sample(&buf, &image, x, y + scale, scale);
             set_fg(r, g, b);
             print!("▄");
         }
@@ -79,9 +78,9 @@ fn main() {
     }
 
     let last_y = last_y + 2 * scale;
-    if last_y + scale < info.height {
-        for x in (0..info.width - scale).step_by(scale as usize) {
-            let (r, g, b) = sample(&buf, &info, x, last_y, scale);
+    if last_y + scale < image.height() {
+        for x in (0..image.width() - scale).step_by(scale as usize) {
+            let (r, g, b) = sample(&buf, &image, x, last_y, scale);
             set_fg(r, g, b);
             print!("▀");
         }
